dc4e787146a146e64c8752c44f3c16b87b77fe5d
Marc Hanheide <marc@hanheide.net>
2021-11-07 11:47:37 +0000

    deal with unstable network connection
    * ensure loop continues even after network failure
    * set session to NOT clean and use client_id (to maintain subscription)
    * set corresponding ROS params to ensure we always have a valid client_id and a not clean session

--- scripts/mqtt_bridge_node.py
+++ scripts/mqtt_bridge_node.py
@@ -2,10 +2,29 @@
 # -*- coding: utf-8 -*-
 import rospy
 
-from mqtt_bridge.app import mqtt_bridge_node
+from uuid import getnode, uuid4
+from mqtt_bridge.app import MqttBridgeNode
 
 
-try:
-    mqtt_bridge_node()
-except rospy.ROSInterruptException:
-    pass
+rospy.init_node('mqtt_bridge_node')
+
+# set the client_id to the mac address + something unique
+rospy.set_param(
+    '~mqtt/client/client_id',
+    (
+        rospy.get_name() 
+        + '_'
+        + ':'.join('%02x' % ((getnode() >> 8*i) & 0xff) for i in reversed(range(6)))
+        + '_' + str(uuid4())
+    )
+)
+
+rospy.set_param(
+    '~mqtt/client/clean_session', 
+    False
+)
+
+
+node = MqttBridgeNode()
+
+node.run()
